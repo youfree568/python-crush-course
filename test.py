aab4a5e2244d566ad48b2944bc55b1812cf532ed
youfree568 <youfree568@gmail.com>
2025-04-25 23:30:31 +0100

    add hello and test

---
+++ test.py
@@ -0,0 +1,20 @@
+class Restaurant:
+	"""опис ресторану"""
+
+	def __init__(self, restaurant_name, cuisine_type):
+		"""ініціалізувати атрибути"""
+		self.name = restaurant_name
+		self.type = cuisine_type
+
+	def describe_restaurant(self):
+		print(f"This is '{self.name}' restaurant!")
+		print(f"He have kitchen of {self.type}.")
+
+	def open_restaurant(self):
+		print(f"Now, restaurant '{self.name}' is open!")
+
+res = Restaurant("Пузата хата", "український фастфуд")
+
+print(res.name, res.type)
+res.describe_restaurant()
+res.open_restaurant()
