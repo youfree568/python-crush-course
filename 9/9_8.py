6e4dcfe8f677d1ef577a0f10d8bdf753ae45f0b1
youfree568 <youfree568@gmail.com>
2025-04-26 22:34:30 +0100

    update 9_8.py

--- 9/9_8.py
+++ 9/9_8.py
@@ -1,45 +1,65 @@
 class User:
-	"""опис користувача"""
-	def __init__(self, first_name):
-		""" initialization"""
+	"""take user information"""
+	def __init__(self, first_name, surname, age, sex):
+		"""initialization"""
 		self.first_name = first_name
+		self.surname =surname
+		self.age = age
+		self.sex = sex
 		self.login_attempts = 0
 
 	def describe_user(self):
-		print(f"This is {self.first_name}") 
+		"""shows usef info"""
+		print(
+			f"User info:\n\tfirst name: {self.first_name}"
+			f"\n\tsurname: {self.surname}\n\tage: {self.age}\n\tsex: {self.sex}\n"
+			) 
 
 	def greet_user(self):
-		print(f"Hello, {self.first_name}")
+		"""user greeting"""
+		print(f"Hello {self.first_name}, nice to see you!\n")	
 
 	def increment_login_attempts(self):
-		"""Increment the value of login_attempts"""
+		"""add trying to login"""
 		self.login_attempts += 1
+		return self.login_attempts
 
 	def reset_login_attempts(self):
-		"""reset login_attempts to 0"""
+		"""reset login attemps to '0' """
 		self.login_attempts = 0
+		return self.login_attempts
 
 
 class Admin(User):
-	"""initialization """
-	def __init__(self, first_name):
-		"""наслідування User"""
-		super().__init__(first_name)
-		"""Екземпляр (клас) Privileges як атрибут для класу адмін"""
+	"""This is SuperUser"""
+	def __init__(self, first_name, surname, age, sex):
+		super().__init__(first_name, surname, age, sex)
+		
+		"""initialization list of privileges"""
 		self.privileges = Privileges()
 
 	
 class Privileges:
-	"""клас який показує переваги"""
-	def __init__(self, privi=[]):
-		self.privi = privi
+	"""Show priviliges"""
+	def __init__(self, privileges=[]):
+		self.privileges = privileges
 
 	def show_privileges(self):
-		"""show the user what the Admin can do"""
-		for privilege in self.privi:
-			print(f"\t- {privilege}")
+		"""show preveligious"""
+		if self.privileges:
+			print("\nUser have Privileges:")
+			for privilege in self.privileges:
+				print(f"\t- {privilege}")
+		else:
+			print("This user has no privileges!")
 
 
-nick = Admin('Admin')
-nick.privileges.privi = ('can add post', 'can delete post', 'can ban user')
-nick.privileges.show_privileges()
+super_user = Admin('Mykola', 'Admin', 33, 'male')
+
+# super_user.describe_user()
+super_user.greet_user()
+super_user.privileges.show_privileges()
+super_user.privileges.privileges = ['can add post', 'can delete post', 'can ban user']
+super_user.privileges.show_privileges()
+
+
