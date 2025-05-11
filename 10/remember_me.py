f72f0556d08dc167a5a484c14837108d13c31bea
youfree568 <youfree568@gmail.com>
2025-05-11 20:24:32 +0100

    modified remember_me.py

--- 10/remember_me.py
+++ 10/remember_me.py
@@ -11,16 +11,21 @@ def get_stored_username():
 	else:
 		return username
 
+def get_new_username():
+	"""asked username"""
+	username = input("What is your name? ")
+	filename = 'json_files\\username.json'	
+	with open(filename, 'w') as f:
+		json.dump(username, f)
+	return username
+
 def greet_user():
 	"""greeting user"""
 	username = get_stored_username()
 	if username:
 		print(f"Welcome back, {username.title()}")
 	else:
-		username = input("What is your name? ")
-		filename = 'json_files\\username.json'	
-		with open(filename, 'w') as f:
-			json.dump(username, f)
-			print(f"We'll remember you when you come back, {username.title()}!")
+		username = get_new_username()
+		print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
